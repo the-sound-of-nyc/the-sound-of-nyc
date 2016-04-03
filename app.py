8d4558763de7a53b7be41f3cdd58736c66655cc6
Yasmeen Roumie <y4smeen@gmail.com>
2016-04-03 07:42:22 -0400

    fix endpoints again

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import utils
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-@app.route('/frequency/<frequency>')
+api.add_resource(HelloWorld, '/')
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+@app.route('/frequency/<int:frequency>')
 def frequency(frequency):
     return render_template("index.html",input=frequency)
 
