9e53ca770e338ba097ab692110d768d111c59bfe
Yasmeen Roumie <y4smeen@gmail.com>
2016-04-03 09:39:27 -0400

    i tried

--- final.py
+++ final.py
@@ -7,22 +7,58 @@ api = Api(app)
 
 users = {}
 
-class Magic(Resource):
+# class Magic(Resource):
+#     def get(self, user_id):
+#         return {user_id: users[user_id]}
+#
+#     def put(self, user_id):
+#         users[user_id][0] = request.form['f'] # 'frequency'
+#         users[user_id][1] = request.form['g'] # 'genre'
+#         users[user_id][2] = request.form['d'] # 'demographics'
+#         users[user_id][3] = request.form['s'] # 'sound_file'
+#
+#         return {user_id: hello[user_id][0],
+#                 user_id: hello[user_id][1],
+#                 user_id: hello[user_id][2],
+#                 user_id: hello[user_id][3]}#{user_id: users[user_id]}
+#
+# api.add_resource(Magic, '/<user_id>')
+
+hello = {}
+
+class Frequency(Resource):
     def get(self, user_id):
-        return {user_id: users[user_id]}
+        return {user_id: hello[user_id]}
 
     def put(self, user_id):
-        users[user_id][0] = request.form['f'] # 'frequency'
-        users[user_id][1] = request.form['g'] # 'genre'
-        users[user_id][2] = request.form['d'] # 'demographics'
-        users[user_id][3] = request.form['s'] # 'sound_file'
+        hello[user_id] = request.form['freq']
+        return {user_id: hello[user_id]}
+class Genre(Resource):
+    def get(self, user_id):
+        return {user_id: hello[user_id]}
 
-        return {user_id: hello[user_id][0],
-                user_id: hello[user_id][1],
-                user_id: hello[user_id][2],
-                user_id: hello[user_id][3]}#{user_id: users[user_id]}
+    def put(self, user_id):
+        hello[user_id] = request.form['genre']
+        return {user_id: hello[user_id]}
+class People(Resource):
+    def get(self, user_id):
+        return {user_id: hello[user_id]}
 
-api.add_resource(Magic, '/<user_id>')
+    def put(self, user_id):
+        hello[user_id] = request.form['people']
+        return {user_id: hello[user_id]}
+class Sound(Resource):
+    def get(self, user_id):
+        return {user_id: hello[user_id]}
+
+    def put(self, user_id):
+        hello[user_id] = request.form['sound']
+        return {user_id: hello[user_id]}
+api.add_resource(Frequency, '/<user_id>')
+api.add_resource(Genre, '/<user_id>')
+api.add_resource(People, '/<user_id>')
+api.add_resource(Sound, '/<user_id>')
+
 
 # # api.add_resource(HelloWorld, '/<frequency>')
 #
