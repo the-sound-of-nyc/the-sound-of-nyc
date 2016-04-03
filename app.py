93f27d50767d7cd6ae9a70302ca41e8eeab89900
Yasmeen Roumie <y4smeen@gmail.com>
2016-04-03 09:47:22 -0400

    i tried

--- app.py
+++ app.py
@@ -17,43 +17,6 @@ class Frequency(Resource):
 
 
 api.add_resource(Frequency, '/<user_id>')
-# api.add_resource(HelloWorld, '/<frequency>')
-
-# @app.route("/")
-# def home():
-#     return render_template("index.html")
-
-# @app.route('/frequency/<int:frequency>')
-# def frequency(frequency):
-#     return render_template("index.html",input=frequency)
-
-@app.route('/genre/<genre>')
-def genre(genre):
-    return render_template("index.html", input=genre)
-
-@app.route('/person/<person>')
-def person(person):
-    return render_template("index.html", input=person)
-
-@app.route('/music/<music>')
-def music(music):
-    return render_template("index.html", input=music)
-
-## Checks the username and password with the utils function auth()
-@app.route('/login', methods=['GET','POST'])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        userid = utils.auth(username, password)
-        if userid != -1:
-            session['logged_in'] = True
-            session['userid'] = userid
-            return redirect(url_for('home'))
-        else:
-            return render_template("login.html", err="Incorrect password or username")
-    else:
-        return render_template("login.html")
 
 if __name__ == "__main__":
     app.debug = True
